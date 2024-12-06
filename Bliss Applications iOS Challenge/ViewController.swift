de9d6c1f45805fc5e340cdfec96c780884969d66
danielleseragioli <danielle.seragioli@gmail.com>
2024-12-06 19:44:10 +0000

    Initial Commit

---
+++ Bliss Applications iOS Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bliss Applications iOS Challenge
+//
+//  Created by Danielle Seragioli on 06/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
